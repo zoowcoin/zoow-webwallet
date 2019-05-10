4cf095b744a2210d9d5db9eed7f4430a50142ca8
ZOOW <47944643+zoowcoin@users.noreply.github.com>
2019-05-10 12:56:01 +0200

    Add files via upload

---
+++ settings.php
@@ -0,0 +1,28 @@
+<?php
+
+$server_url = "/";  // ENTER WEBSITE URL ALONG WITH A TRAILING SLASH
+
+$db_host = "localhost";
+$db_user = "wallet_user";
+$db_pass = "4fKigzWR7veDPZlvU1dVzYg4H";
+$db_name = "wallet";
+
+$rpc_host = "127.0.0.1";
+$rpc_port = "8491";
+$rpc_user = "rpc_zoow";
+$rpc_pass = "UikEppUzKNbR7tGFKPMq3zhlS3eBUp2f";
+
+$fullname = "ZOOW"; //Website Title (Do Not include 'wallet')
+$short = "ZOW"; //Coin Short (BTC)
+$blockchain_tx_url = "http://zoowexplorer.site:3001/tx/"; //Blockchain Url
+$support = "info@zoow.io"; //Your support eMail
+$hide_ids = array(1); //Hide account from admin dashboard
+$donation_address = "ZPHbvVmhQJ8Ji8mbXcVr5ePpTZ3qCQu7kk"; //Donation Address
+
+$reserve = "0"; //This fee acts as a reserve. The users balance will display as the balance in the daemon minus the reserve. We don't reccomend setting this more than the Fee the daemon charges.
+//Recaptcha
+$public = "6LckX6IUAAAAAFckUY_s8rf-2WtsB7zCTK-bwDQW";
+$secret = "6LckX6IUAAAAAAiL_IqerDJ4pe4IHuzwf4W5JELn";
+
+
+?>
